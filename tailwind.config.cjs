705b1ffe6a1627c89a4c03c0c53bc079cf015470
Noisk8 <juansk8jaramillo@gmail.com>
2023-06-14 11:37:00 -0500

    cambios 1

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+
+      colors: {
+        //white: "#8f9197",
+
+        secondary: "#493977",
+        primary: "#fafafa",
+      },
+      fontFamily: {
+        sans: ["Neue Haas Grotesk Text Pro", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+
+}
